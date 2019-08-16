b17c2435d60952e0be296eaf2e4eab06b6c40d33
Vikas Pawar <vikaspawar2112@gmail.com>
2019-08-16 11:06:32 +0530

    CR:Vikas Add fullscreen view activity.

--- app/src/main/java/com/sundaymobility/FullScreenView.kt
+++ app/src/main/java/com/sundaymobility/FullScreenView.kt
@@ -1,4 +1,13 @@
 package com.sundaymobility
 
-class FullScreenView {
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class FullScreenView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fullscreen_view)
+
+    }
 }
